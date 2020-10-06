f2ece611b41debd0e4ee91ebf52ee967c9634289
David Sulava <sulavaperm@gmail.com>
2020-10-06 22:57:02 +0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Demo_Anime_React_Front_End/precache-manifest.05a8715b8a823436a1ad7d5865fc0312.js"
+  "/Demo_Anime_React_Front_End/precache-manifest.af7188150e54e2d4e0b388f1c3b92f39.js"
 );
 
 self.addEventListener('message', (event) => {
