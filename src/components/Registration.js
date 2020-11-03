a4416e766d20f892f46c5922590fb48972be3a65
David Sulava <sulavaperm@gmail.com>
2020-11-03 15:23:07 +0500

    refacoting

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -5,22 +5,31 @@ import  Recaptcha           from 'react-recaptcha'
 
 import { getUser } from '../store/actions/getUser'
 
+let recaptchaInstance = null
 
 export class Registration extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { ...props, form: { 'name': '', 'email': '', 'firstName': '', 'lastName': '',  'phone': '', 'password': '', 'password_confirmation':'' } };
-
-        this.valueChanged = this.valueChanged.bind(this);
-        this.submForm     = this.submForm.bind(this);
+        this.state = {
+          ...props,
+          form : {
+            'name'                  : '',
+            'email'                 : '',
+            'firstName'             : '',
+            'lastName'              : '',
+            'phone'                 : '',
+            'password'              : '',
+            'password_confirmation' :''
+          }
+        };
     }
 
     componentDidMount( ){
       this.props.dellMessages();
     };
 
-    valueChanged(ev){
+    valueChanged = (ev)=>{
 
       var ch_key = ev.target.name;
       var ch_val = ev.target.value
@@ -28,79 +37,86 @@ export class Registration extends Component {
       this.setState( { form: { ...this.state.form, [ ch_key ]: ch_val } } );
     };
 
-    submForm(ev){
+    submForm = (ev)=>{
       ev.preventDefault()
       var path =  '/users/register'
 
       var formData = new FormData( ev.target );
 
+      recaptchaInstance.reset();
+
       this.props.getData( path, formData );
     }
 
-    localSection()
-      {
-        if( this.props.msg && this.props.msg.loginSuccess )
-          {
-            this.props.history.push("/");
-            return null
-          }
-        else if( this.props.msg && this.props.msg.regSuccess )
-          {
-            var wrapperStyle = { height: '100vh', margin: '20px auto' };
-            var alertStyle   = { padding: '20px' }
-            var element = ( <div style={ wrapperStyle }> <div style={ alertStyle }  className="alert alert-success" role="alert">{ this.props.msg.regSuccess } </div> </div>);
+    localSection = ()=> {
 
-            (function( props )
-              {
-                setTimeout( ()=>{  props.history.push("/");  }, 3000);
-              }
-            )(this.props)
+      if( this.props.msg && this.props.msg.loginSuccess )
+        {
+          this.props.history.push("/");
+          return null
+        }
+      else if( this.props.msg && this.props.msg.regSuccess )
+        {
+          var wrapperStyle = { height: '100vh', margin: '20px auto' };
+          var alertStyle   = { padding: '20px' }
+          var element = ( <div style={ wrapperStyle }> <div style={ alertStyle }  className="alert alert-success" role="alert">{ this.props.msg.regSuccess } </div> </div>);
+
+          (function( props )
+            {
+              setTimeout( ()=>{  props.history.push("/");  }, 3000);
+            }
+          )(this.props)
 
 
-            return  element;
-          }
-        else
-          {
-            return (
+          return  element;
+        }
+      else
+        {
+          return (
 
-                <form onSubmit={ this.submForm }  method = "POST" className="regForm" action='users/register'>
-                    <div className="Registration">
-                        { ( this.props.msg && this.props.msg.errorCred ) && <div className="alert alert-danger">{ this.props.msg.errorCred } </div> }
-                        <input type="hidden" name='sender'   value='' /> <br/>
+              <form onSubmit={ this.submForm }  method = "POST" className="regForm" action='users/register'>
+                  <div className="Registration">
+                      { ( this.props.msg && this.props.msg.errorCred ) && <div className="alert alert-danger">{ this.props.msg.errorCred } </div> }
+                      <input type="hidden" name='sender'   value='' /> <br/>
 
-                        <input type="text"  onChange={this.valueChanged} name='name'      className="username"  placeholder="Username"  value= { this.state.form.username }  /><br/>
-                        { ( this.props.msg && this.props.msg.errorUsername) && <div className="alert alert-danger">{ this.props.msg.errorUsername } </div> }
+                      <input type="text"  onChange={this.valueChanged} name='name'      className="username"  placeholder="Username"  value= { this.state.form.username }  /><br/>
+                      { ( this.props.msg && this.props.msg.errorUsername) && <div className="alert alert-danger">{ this.props.msg.errorUsername } </div> }
 
-                        <input type="text"  onChange={this.valueChanged} name='email'     className="email"     placeholder="E-mail"     value= { this.state.form.email } / ><br/>
-                        { ( this.props.msg && this.props.msg.emailErr) && <div className="alert alert-danger">{ this.props.msg.emailErr } </div> }
+                      <input type="text"  onChange={this.valueChanged} name='email'     className="email"     placeholder="E-mail"     value= { this.state.form.email } / ><br/>
+                      { ( this.props.msg && this.props.msg.emailErr) && <div className="alert alert-danger">{ this.props.msg.emailErr } </div> }
 
-                        <input type="text"  onChange={this.valueChanged} name='firstName' className="firstName" placeholder="First Name" value= { this.state.form.firstName } /><br/>
+                      <input type="text"  onChange={this.valueChanged} name='firstName' className="firstName" placeholder="First Name" value= { this.state.form.firstName } /><br/>
 
-                        <input type="text"  onChange={this.valueChanged} name='lastName'  className="lastName"  placeholder="Last Name"  value= { this.state.form.lastName } /><br/>
+                      <input type="text"  onChange={this.valueChanged} name='lastName'  className="lastName"  placeholder="Last Name"  value= { this.state.form.lastName } /><br/>
 
-                        <input type="text"  onChange={this.valueChanged} name='phone'     className="phone"     placeholder="Phone Number" value= { this.state.form.phone } /><br/><br/>
+                      <input type="text"  onChange={this.valueChanged} name='phone'     className="phone"     placeholder="Phone Number" value= { this.state.form.phone } /><br/><br/>
 
-                        <input type="password" onChange={this.valueChanged} name='password' placeholder="Password"  className="password" value= { this.state.form.password}></input>
-                        { ( this.props.msg && this.props.msg.errorPassword)  && <div className="alert alert-danger">{ this.props.msg.errorPassword } </div> }
+                      <input type="password" onChange={this.valueChanged} name='password' placeholder="Password"  className="password" value= { this.state.form.password}></input>
+                      { ( this.props.msg && this.props.msg.errorPassword)  && <div className="alert alert-danger">{ this.props.msg.errorPassword } </div> }
 
-                        <input type="password" onChange={this.valueChanged} name='password_confirmation'  placeholder="Confirm Password" className="pasConfirm" value= { this.state.form.password_confirmation} />
-                        { ( this.props.msg && this.props.msg.errorPass_confirm ) && <div className="alert alert-danger">{ this.props.msg.errorPass_confirm } </div> }
-                        <br/>
-                        { (  this.props.msg && this.props.msg.regSuccess && !this.props.errorCred) && <div className="alert alert-success" role="alert">{ this.props.regSuccess } </div> }
+                      <input type="password" onChange={this.valueChanged} name='password_confirmation'  placeholder="Confirm Password" className="pasConfirm" value= { this.state.form.password_confirmation} />
+                      { ( this.props.msg && this.props.msg.errorPass_confirm ) && <div className="alert alert-danger">{ this.props.msg.errorPass_confirm } </div> }
+                      <br/>
+                      { (  this.props.msg && this.props.msg.regSuccess && !this.props.errorCred) && <div className="alert alert-success" role="alert">{ this.props.regSuccess } </div> }
 
-                        <br/>
-                        { (this.props.msg && this.props.msg.erCaptcha) && <div className="alert alert-danger">{ this.props.msg.erCaptcha } </div> }
-                        <Recaptcha sitekey={ process.env.REACT_APP_RECAPCHA_KEY  } render="explicit" hl={window.navigator.userLanguage || window.navigator.language} />
-                        <br/>
+                      <br/>
+                      { (this.props.msg && this.props.msg.erCaptcha) && <div className="alert alert-danger">{ this.props.msg.erCaptcha } </div> }
+                      <Recaptcha
+                        sitekey = { process.env.REACT_APP_RECAPCHA_KEY  }
+                        render  = "explicit"
+                        hl      = { window.navigator.userLanguage || window.navigator.language }
+                        ref     = { e => recaptchaInstance = e }
+                      />
 
-                        <button type="submit"  name="submitReg" className="submitRreg" >SUBMIT</button>
-                    </div>
+                      <br/>
+                      <button type="submit"  name="submitReg" className="submitRreg" >SUBMIT</button>
+                  </div>
 
-                </form>
+              </form>
 
-            )
-          }
-      }
+          )
+        }
+    }
     render(){
       return (
         <div>
